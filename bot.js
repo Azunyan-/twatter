501b5d9e698f43620a2b05ebd7f6fe51a617d57d
Azunyan- <panton123@gmail.com>
2018-05-14 00:11:52 +0200

    test tweet

--- bot.js
+++ bot.js
@@ -4,3 +4,17 @@ var Twit = require('twit');
 
 var config = require('./config');
 var T = new Twit(config);
+
+var tweet = {
+    status: 'test from  node'
+}
+
+T.post('statuses/update', tweet, tweeted);
+
+function tweeted(err, data, response) {
+    if(err) {
+        console.log("Something went wrong!");
+    } else {
+        console.log("it worked!");
+    }
+}

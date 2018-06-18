fe679558b40aee169c349232c504f84d739ed684
Azunyan- <panton123@gmail.com>
2018-06-19 00:29:29 +0200

    Working code, working tweets and readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # twatter
+
+Launch the bot with
+``` node index.js
+```
+Setup a file called `config.js` according to `config-sample.js` to make sure that it is able to tweet.
+
+Since we aren't using databases the last post title is simply saved in the file `lastPostTitle.txt`, really lazy solution but this is meant to be hosted from a lone raspberry pi.
+
+It currently does not run on a loop so I am planning on making a cron job that simply executes it regularly, once an hour?
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const request = require('request');
 const cheerio = require('cheerio');
-const fs = ('fs');
+const fs = require('fs');
+const Twit = require('twit');
 
+var config = require('./config');
+var T = new Twit(config);
 /*
 const options = {
     uri: 'https://sl.se/sv/find/',
@@ -18,19 +21,84 @@ rp(options)
         console.log(err);
     });
 */
+var firstArticle = 0;
+var savedTitle;
+fs.readFile('lastPostTitle.txt', 'utf8', function(err, contents) {
+    console.log("Saved article name: ", contents);
+    savedTitle = contents;
+});
+
 request('https://sl.se/sv/find/', function (error, response, html) {
   if (!error && response.statusCode == 200) {
     var $ = cheerio.load(html);
     $('article.expandable').each(function(i, element){
         var header = $(this).children('header').children('h1').children('span');
-        console.log(header.text());
-        var shorttext = $(this).children('.short-text');
-        var link = $(header).attr('data-url');
-        console.log(shorttext.text());
-        console.log(link);
+
+        if(firstArticle == 0){
+            //console.log(header.text());
+            var title = header.text();
+            var shorttext = $(this).children('.short-text').text();
+            var link = $(header).attr('data-url');
+            //console.log(shorttext);
+            //console.log(link);
+
+            if(header.text() == savedTitle){
+                console.log("No new article");
+            } else {
+
+                //String fixy bit
+                title = title.trim();
+                shorttext = shorttext.trim();
+                link = link.trim();
+
+                //Tweety bit
+                var tweetcontent = title + " " + shorttext + " " + link;
+                var tweet = {
+                    status: tweetcontent
+                }
+                //console.log(tweet);
+
+                //Check for length issues
+                var tweetLength = tweetcontent.length;
+                if( tweetLength <= 280){
+                    console.log('Automatic composed tweet length under 280')
+
+                    T.post('statuses/update', tweet, tweeted);
+
+                    function tweeted(err, data, response) {
+                        if(err) {
+                            console.log("Something went wrong whilst tweeting!");
+                        } else {
+                            console.log("Tweet success!");
+                        }
+                    }
+
+                } else {
+                    console.log('Automatic tweet was to long: ', tweetLength);
+                }
+                //Article title save bit
+                firstArticle = 1;
+                fs.writeFile("lastPostTitle.txt", header.text(), function(err) {
+                    if(err) {
+                        console.log(err);
+                    } else {
+                        console.log("file has been saved successfully");
+                    }
+                });
+
+            }
+        }
+
+
+
+
     });
+
   }
 });
+
+
+
 /*
 request('https://news.ycombinator.com', function (error, response, html) {
   if (!error && response.statusCode == 200) {
